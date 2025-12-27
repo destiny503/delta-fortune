ef28bc978a16c0fc0a1d308a23a5c28ff5d43bd8
destiny503 <503destiny503@gmail.com>
2025-12-27 06:58:54 +0400

    fix: prisma+amvera

--- src/prisma.js
+++ src/prisma.js
@@ -2,4 +2,9 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+prisma
+  .$connect()
+  .then(() => console.log("✅ Connected to DB"))
+  .catch((e) => console.error("❌ DB connection error:", e));
+
 export default prisma;
